c7f13103cca9ef88add64d2c3211230679080c3b
Zorin <zorin1477@gmail.com>
2022-03-22 22:06:36 +0800

    feat: cache for SSR

--- src/vite.ts
+++ src/vite.ts
@@ -1,31 +1,41 @@
 import { Plugin, ResolvedConfig } from 'vite';
 import { transform } from './core';
+import { CodeCache } from './core/utils';
 
-interface Store {
-    config: ResolvedConfig | null;
+interface PluginOptions {
+    clean?: boolean;
 }
 
-export default function VitePluginVueTypeImports(): Plugin {
-    const store: Store = {
-        config: null,
-    };
+export default function VitePluginVueTypeImports(options: PluginOptions = {}): Plugin {
+    const clean = options.clean ?? false;
+    let resolvedConfig: ResolvedConfig | undefined;
+    // NOTE: It might be useful for SSR?
+    const cache = new CodeCache();
 
     return {
         name: 'vite-plugin-vue-type-imports',
         enforce: 'pre',
         async configResolved(config) {
-            store.config = config;
+            resolvedConfig = config;
         },
         async transform(code, id) {
             if (!/\.(vue)$/.test(id)) return;
 
-            const aliases = store.config?.resolve.alias;
+            const aliases = resolvedConfig?.resolve.alias;
+
+            const transformedCode =
+                cache.get(id) ??
+                cache.set(
+                    id,
+                    await transform(code, {
+                        id,
+                        aliases,
+                        clean,
+                    }),
+                );
 
             return {
-                code: await transform(code, {
-                    id,
-                    aliases,
-                }),
+                code: transformedCode,
             };
         },
     };
