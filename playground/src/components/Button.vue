92ddc10994f1514fc10e4367eb9fb03eae78f9fe
Zolyn <zorin1477@gmail.com>
2022-05-09 00:06:12 +0800

    chore: playground

--- playground/src/components/Button.vue
+++ playground/src/components/Button.vue
@@ -1,25 +1,16 @@
 <script setup lang="ts">
 import { ButtonProps } from '~/button-types';
 
-// type A = string;
+// const df = {
+//     size: () => ({ msg: '1', base: true }),
+//     color: 'blue',
+// };
 
-// interface Size {
-//     small: 'sm';
-//     medium: 'md';
-//     large: 'lg';
-// }
+interface BProps extends ButtonProps {
+    more: boolean;
+}
 
-// interface ButtonProps {
-//     color: string;
-//     size: number | Size | A;
-// }
-
-const df = {
-    size: () => ({ msg: '1', base: true }),
-    color: 'blue',
-};
-
-const props = withDefaults(defineProps<ButtonProps>(), {
+const props = withDefaults(defineProps<BProps>(), {
     size: () => ({ msg: '1', base: true }),
     color: 'blue',
 });
