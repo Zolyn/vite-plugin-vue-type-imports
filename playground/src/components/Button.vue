407743e626045062ded7a1e79d428f6f12b468ec
Zolyn <zorin1477@gmail.com>
2022-05-11 01:57:07 +0800

    test: playground

--- playground/src/components/Button.vue
+++ playground/src/components/Button.vue
@@ -10,15 +10,12 @@ interface BProps extends ButtonProps {
     more: boolean;
 }
 
-const props = withDefaults(defineProps<BProps>(), {
-    size: () => ({ msg: '1', base: true }),
-    color: 'blue',
-});
+const props = defineProps<BProps>();
 </script>
 
 <template>
     <button py-2 px-4 font-medium rounded m-8>
-        {{ props.color }}
+        {{ props.more }}
         <slot />
     </button>
 </template>
